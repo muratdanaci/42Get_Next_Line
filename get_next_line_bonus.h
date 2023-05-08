d1ca92cd64a954dc0eacad1b205a242852c91dd8
Murat Danaci <muratdanaci8@gmail.com>
2023-05-08 16:21:43 +0300

    .

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mudanaci <mudanaci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 16:18:46 by mudanaci          #+#    #+#             */
+/*   Updated: 2023/05/08 16:19:25 by mudanaci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char *buf);
+char	*get_next_line(int fd);
+char	*get_new_line(char *buffer);
+char	*first_read(int fd, char *buffer);
+int		find_nl_char(char *buffer);
+char	*strjoin(char *buffer, char *new_buffer);
+char	*get_new_buffer(char *buffer);
+
+#endif
